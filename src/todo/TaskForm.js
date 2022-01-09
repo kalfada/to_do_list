d5206de46810af6de282a98457c3e8e9e93285f5
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2022-01-09 09:52:22 +0200

    remove counter from TaskForm

--- src/todo/TaskForm.js
+++ src/todo/TaskForm.js
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import '../css/TaskForm.css'
 
 function TaskForm() {
-    const [counter, setCounter] = useState(0)
+    // const [counter, setCounter] = useState(0)
 
-    function clicked(event) {
-        event.preventDefault()
-        setCounter(counter + 1)
-    }
-
+    // function clicked(event) {
+    //     event.preventDefault()
+    //     setCounter(counter + 1)
+    // }
+//onSubmit={clicked}
     return (
-        <form action="" onSubmit={clicked}>
+        <form action="" > 
             <input type="text" name="" id="" className="textInput" />
             <input type="submit" value="+" className="submit_btn"/>
         </form>
